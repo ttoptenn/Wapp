a029d5d24cd675c2bc159dcfe8b371d79ef3738f
Jantharat Chumsang <jantharatchjune@gmail.com>
2025-05-18 14:16:51 +0700

    Update member_app.py

--- apps/member_app.py
+++ apps/member_app.py
@@ -22,6 +22,6 @@ class MemberApp(HydraHeadApp):
 
         # footer
         # st.markdown("""---""")
-        st.image('resources/memb.png', width=300, height=200, clamp=False, channels="RGB", output_format="auto")
+        st.image('resources/memb.png', width=300, clamp=False, channels="RGB", output_format="auto")
 
     
