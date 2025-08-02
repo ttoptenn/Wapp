e96d94b5e42460c4c4ddcffb905e143926bc23f0
Nattaporn  Wiriyaprapanont <75828161+ttoptenn@users.noreply.github.com>
2025-08-02 15:27:30 +0700

    Update dashb_app.py

--- apps/dashb_app.py
+++ apps/dashb_app.py
@@ -22,21 +22,22 @@ class DashbApp(HydraHeadApp):
             page =page_file.read()
             stc.html(page,width=width, height=height , scrolling = False)
         st_webpage('apps/powerBI.html')
-        pdf_path = Path("apps/Handbook for dashboard.pdf")
+        
 
-        if pdf_path.exists():
-            col1, col2 = st.columns([1,1])
-            with col1:
-                if st.button("👁️ ลองดู Handbook ในเว็บ (อาจถูก Chrome บล็อก)"):
-                    with open(pdf_path, "rb") as f:
-                        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-                    pdf_display = f"""
-                        <iframe src='data:application/pdf;base64,{base64_pdf}' width='800' height='1000' type='application/pdf'></iframe>
-                    """
-                    st.markdown("### 📑 Preview Handbook")
-                    st.markdown(pdf_display, unsafe_allow_html=True)
-            with col2:
-                with open(pdf_path, "rb") as f:
-                    st.download_button("📥 ดาวน์โหลด Handbook (PDF)", f, file_name="Handbook for dashboard.pdf")
-        else:
-            st.warning("ไม่พบไฟล์ Handbook for dashboard.pdf")
+with tab2:
+    pdf_path = Path("apps/Handbook for dashboard.pdf")
+
+    if pdf_path.exists():
+        with open(pdf_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+        pdf_display = f"""
+            <iframe src='data:application/pdf;base64,{base64_pdf}' width='800' height='1000' type='application/pdf'></iframe>
+        """
+        st.markdown("### 📑 Handbook Preview")
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+        with open(pdf_path, "rb") as f:
+            st.download_button("📥 ดาวน์โหลด Handbook", f, file_name="Handbook for dashboard.pdf")
+    else:
+        st.warning("ไม่พบไฟล์ Handbook for dashboard.pdf")
