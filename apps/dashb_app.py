6b76e99656147abe3223bfe27fd2b203d3a704b3
Nattaporn  Wiriyaprapanont <75828161+ttoptenn@users.noreply.github.com>
2025-08-02 15:34:17 +0700

    Update dashb_app.py

--- apps/dashb_app.py
+++ apps/dashb_app.py
@@ -25,17 +25,12 @@ class DashbApp(HydraHeadApp):
         
         pdf_path = Path("apps/Handbook for dashboard.pdf")
         st.markdown(f'<a href="Handbook for dashboard.pdf" target="_blank">📖 เปิด Handbook ในแท็บใหม่</a>', unsafe_allow_html=True)
-        if pdf_path.exists():
-            with open(pdf_path, "rb") as f:
-                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    
-            pdf_display = f"""
-                <iframe src='data:application/pdf;base64,{base64_pdf}' width='800' height='1000' type='application/pdf'></iframe>
-            """
-            st.markdown("### 📑 Handbook Preview")
-            st.markdown(pdf_display, unsafe_allow_html=True)
-    
-            with open(pdf_path, "rb") as f:
+        pdf_file = "Handbook for dashboard.pdf"
+        
+            st.markdown(
+                f'<a href="{pdf_file}" target="_blank">📖 เปิด Handbook ในแท็บใหม่</a>',
+                unsafe_allow_html=True
+            )
+        
+            with open(pdf_file, "rb") as f:
                 st.download_button("📥 ดาวน์โหลด Handbook", f, file_name="Handbook for dashboard.pdf")
-        else:
-            st.warning("ไม่พบไฟล์ Handbook for dashboard.pdf")
